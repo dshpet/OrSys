e97c87dc3cb21045b66e42f405d17a7490e27490
dshpet <dshpet@gmail.com>
2015-05-11 14:42:14 +0300

    initialized and done project structure

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://mainUser:simplepassword@ds029541.mongolab.com:29541/orsys'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://mainUser:simplepassword@ds029541.mongolab.com:29541/orsys'
+  }
+};
