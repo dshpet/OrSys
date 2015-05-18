d95fe2b4532fcd7551568a09e773ae62262b3a47
dshpet <dshpet@gmail.com>
2015-05-18 20:32:01 +0300

    finally removed dependecy

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -19,7 +19,6 @@ function onConnect(socket) {
 
   // Insert sockets below
   require('../api/dataReceiver/dataReceiver.socket').register(socket);
-  require('../api/dataReciever/dataReciever.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
