d6b95c00184be545c523ea24204d80b5ea6be3e8
dshpet <dshpet@gmail.com>
2015-05-28 13:11:07 +0300

    google analytics added

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -3,20 +3,23 @@
 angular.module('orSysApp')
   .controller('DashboardCtrl', function ($scope, $http, socket, Auth, User) {
     $scope.message = 'Hello';
-    $scope.user = {};
+    $scope.user;
     $scope.userData = [];
     var current = Auth.getCurrentUser().$promise.then(function(res){
     	$scope.user = res;
 
-      console.log("url : " + '/api/dataReceivers/googleId/' + res.google.id);
-      $http.get('/api/dataReceivers/googleId/' + res.google.id).success(function(dataReceiver) {
-        $scope.userData = dataReceiver;
-        socket.syncUpdates('data', $scope.userData);
-      });
+      updateData();
     }, function(err){
     	console.log(err);
     });
 
-    //$scope.formatTime = function(utcDate) { return moment(utcDate).format("MMddYYYY"); } ;
-    $scope.formatTime1 = function(mongoDate) {return mongoDate.toDateString();};   
+    var updateData = function() {
+      $http.get('/api/dataReceivers/googleId/' + $scope.user.google.id).success(function(dataReceiver) {
+        $scope.userData = dataReceiver;
+        socket.syncUpdates('data', $scope.userData);
+      });
+    };
+    var timer = setInterval(function() {
+      $scope.$apply(updateData);
+    }, 1000);  
   });
